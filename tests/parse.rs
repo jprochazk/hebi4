daa1689d178968786acc21cdc4d2678465d5bbcd
jprochazk <pjanv42@gmail.com>
2025-06-29 14:42:20 +0200

    delete old tests

--- tests/parse.rs
+++
@@ -1,8 +0,0 @@
-use hebi4::{parser::parse, token::tokenize};
-use std::fs::read_to_string;
-
-globdir::globdir!("tests/inputs/*.hi", |path| {
-    let input = read_to_string(path).unwrap();
-    let tokens = tokenize(&input);
-    insta::assert_debug_snapshot!(parse(&tokens).unwrap());
-});
--- tests/token.rs
+++
@@ -1,7 +0,0 @@
-use hebi4::token::tokenize;
-use std::fs::read_to_string;
-
-globdir::globdir!("tests/inputs/*.hi", |path| {
-    let input = read_to_string(path).unwrap();
-    insta::assert_debug_snapshot!(tokenize(&input));
-});
